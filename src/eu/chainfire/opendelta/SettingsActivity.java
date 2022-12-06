6b5dc06b040cff6eb1a3dcfaf51e22f2c49630cc
Ido Ben-Hur <idoybh2@gmail.com>
2022-12-06 08:13:29 +0200

    SettingsActivity: Adapt to android-13.0.0_r16

--- src/eu/chainfire/opendelta/SettingsActivity.java
+++ src/eu/chainfire/opendelta/SettingsActivity.java
@@ -20,7 +20,7 @@ package eu.chainfire.opendelta;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class SettingsActivity extends CollapsingToolbarBaseActivity {
 
