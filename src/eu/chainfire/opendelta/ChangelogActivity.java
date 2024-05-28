4a5574faa03428a014c6ee54b44cc6ab54e7ecb9
Ido Ben-Hur <idoybh2@gmail.com>
2024-05-28 13:22:21 +0300

    OpenDelta: ChangelogActivity: Cache the downloaded changelogs
    in SharedPreferences. this is a small enough data for that
    also enables the user to view the cache offline

--- src/eu/chainfire/opendelta/ChangelogActivity.java
+++ src/eu/chainfire/opendelta/ChangelogActivity.java
@@ -24,6 +24,7 @@ import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -34,6 +35,9 @@ import android.view.ViewGroup.MarginLayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.preference.PreferenceManager;
+
+import java.util.ArrayList;
 import java.util.Locale;
 
 import org.json.JSONArray;
@@ -41,6 +45,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ChangelogActivity extends Activity {
+
+    private static final String CACHE_SHAS_KEY = "changelog_cache_shas";
+    private static final String CACHE_DATES_KEY = "changelog_cache_dates_";
+    private static final String CACHE_LOGS_KEY = "changelog_cache_texts_";
 
     private LinearLayout mChangelogLayout;
     private TextView mLoadingText;
@@ -57,6 +65,7 @@ public class ChangelogActivity extends Activity {
         mLoadingText = findViewById(R.id.loading_text);
 
         Config config = Config.getInstance(this);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         HandlerThread ht = new HandlerThread("ChangelogThread");
         ht.start();
 
@@ -73,13 +82,76 @@ public class ChangelogActivity extends Activity {
                     "Changelog.txt");
             boolean error = false;
             try {
+                String[] prefShas = prefs.getString(CACHE_SHAS_KEY, "").split(";");
+                ArrayList<String> shas = new ArrayList<>();
+                ArrayList<String> dates = new ArrayList<>();
+                ArrayList<String> logs = new ArrayList<>();
+                for (int i = 0; i < prefShas.length; i++) {
+                    shas.add(prefShas[i]);
+                    dates.add(prefs.getString(CACHE_DATES_KEY + i, ""));
+                    logs.add(prefs.getString(CACHE_LOGS_KEY + i, ""));
+                }
                 boolean reached = false;
                 int reachedI = 0;
-                final JSONArray jArr = new JSONArray(Download.asString(config.getUrlAPIHistory()));
+
+                final String jsonStr = Download.asString(config.getUrlAPIHistory());
+                JSONArray jArr = null;
+                if (jsonStr != null && !jsonStr.isEmpty()) {
+                    jArr = new JSONArray(jsonStr);
+                } else {
+                    // we can't fetch new right now. display the cache
+                    if (shas.size() <= 0) {
+                        // we have no cache yet
+                        Logger.d("No changelog cache");
+                        error = true;
+                        return;
+                    }
+                    if (dates.size() != logs.size() || dates.size() != shas.size()) {
+                        // invalidate damaged cache
+                        Logger.d("Damaged changelog cache");
+                        SharedPreferences.Editor editor = prefs.edit();
+                        for (int i = 0; i < prefShas.length; i++) {
+                            editor.remove(CACHE_DATES_KEY + i);
+                            editor.remove(CACHE_LOGS_KEY + i);
+                        }
+                        editor.remove(CACHE_SHAS_KEY);
+                        editor.apply();
+                        error = true;
+                        return;
+                    }
+                    // cache seems valid. display
+                    for (int i = 0; i < dates.size(); i++) {
+                        final Long fileDate = Long.parseLong(dates.get(i));
+                        if (!reached) {
+                            reached = fileDate <= currDate;
+                            reachedI = i;
+                        }
+                        final boolean isCurrent = fileDate == currDate || reached && i == reachedI;
+                        addTitle(dates.get(i), isCurrent);
+                        addText(logs.get(i));
+                    }
+                    return;
+                }
+
+                ArrayList<String> foundShas = new ArrayList<>();
                 for (int i = 0; i < jArr.length() && (i < 20 || !reached); i++) {
                     try {
                         // figure out the title and date
                         final String currSha = jArr.getJSONObject(i).getString("sha");
+                        foundShas.add(currSha);
+                        final int cachedIndex = shas.indexOf(currSha);
+                        if (cachedIndex >= 0) {
+                            // value exists in cache
+                            final Long fileDate = Long.parseLong(dates.get(cachedIndex));
+                            if (!reached) {
+                                reached = fileDate <= currDate;
+                                reachedI = i;
+                            }
+                            final boolean isCurrent = fileDate == currDate || reached && i == reachedI;
+                            addTitle(dates.get(cachedIndex), isCurrent);
+                            addText(logs.get(cachedIndex));
+                            continue;
+                        }
                         final String otaJsonURL = String.format(Locale.ENGLISH, jsURL, currSha);
                         final JSONObject otaJson = new JSONObject(Download.asString(otaJsonURL));
                         final String filename = otaJson.getJSONArray("response")
@@ -96,14 +168,42 @@ public class ChangelogActivity extends Activity {
                             reachedI = i;
                         }
                         final boolean isCurrent = fileDate == currDate || reached && i == reachedI;
-                        addTitle(fileDate.toString(), isCurrent);
+                        final String fileDateStr = fileDate.toString();
+                        addTitle(fileDateStr, isCurrent);
                         addText(currChangelog);
+                        // cache
+                        shas.add(i, currSha);
+                        dates.add(i, fileDateStr);
+                        logs.add(i, currChangelog);
                     } catch (JSONException e) {
                         Logger.ex(e);
                         error = true;
                         break;
                     }
                 }
+
+                // handling cache
+                for (int i = 0; i < shas.size(); i++) {
+                    if (foundShas.contains(shas.get(i)))
+                        continue;
+                    // remove no longer existing / needed sha
+                    shas.remove(i);
+                    dates.remove(i);
+                    logs.remove(i);
+                }
+                // saving the still valid shas
+                StringBuilder shaSB = new StringBuilder(shas.get(0));
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString(CACHE_DATES_KEY + "0", dates.get(0));
+                editor.putString(CACHE_LOGS_KEY + "0", logs.get(0));
+                for (int i = 1; i < shas.size(); i++) {
+                    shaSB.append(";" + shas.get(i));
+                    editor.putString(CACHE_DATES_KEY + i, dates.get(i));
+                    editor.putString(CACHE_LOGS_KEY + i, logs.get(i));
+                }
+                editor.putString(CACHE_SHAS_KEY, shaSB.toString());
+                editor.apply();
+                Logger.d("Cached " + shas.size() + " changelog entries");
             } catch (Exception e) {
                 Logger.ex(e);
                 error = true;
